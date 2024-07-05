b25b888ce8c47c86f415864105ae9a1a9dec9f3c
Weber <henryplainview@mail.com>
2024-07-05 14:16:35 +0300

    Правки по prettier

--- js/functions.js
+++ js/functions.js
@@ -2,34 +2,42 @@ function checkWordLength(string, value = 1) {
   return string.length <= value;
 }
 
-console.log(checkWordLength('проверяемая строка', 20));
+checkWordLength('проверяемая строка', 20);
 
 function isPalindrome(string) {
-	const changedString = string.toLowerCase().replaceAll(' ', '');
-	return changedString === changedString.split('').reverse().join('');
+  const changedString = string.toLowerCase().replaceAll(' ', '');
+  return changedString === changedString.split('').reverse().join('');
 }
 
-console.log(isPalindrome('Лёша на полке клопа нашёл '));
+isPalindrome('Лёша на полке клопа нашёл ');
 
 function getNumber(param) {
-	if (typeof param === 'number') return Number.isInteger(param) ? Math.abs(param) : parseInt(Math.abs(param * 10));
+  if (typeof param === 'number') {
+    return Number.isInteger(param)
+      ? Math.abs(param)
+      : parseInt(Math.abs(param * 10), 10);
+  }
 
-	let number = '';
+  let number = '';
 
-	for(let i = 0; i < param.length; i++) {
-			if(param[i] !== ' ' && typeof Number(param[i]) === 'number' && !isNaN(Number(param[i]))) {
-				number += param[i];
-			}
-	}
+  for (let i = 0; i < param.length; i++) {
+    if (
+      param[i] !== ' ' &&
+      typeof Number(param[i]) === 'number' &&
+      !isNaN(Number(param[i]))
+    ) {
+      number += param[i];
+    }
+  }
 
-	return parseInt(number);
+  return parseInt(number, 10);
 }
 
-console.log(getNumber('2023 год'));
-console.log(getNumber('ECMAScript 2022'));
-console.log(getNumber('1 кефир, 0.5 батона'));
-console.log(getNumber('агент 007'));
-console.log(getNumber('а я томат'));
-console.log(getNumber(true));
-console.log(getNumber(1.5));
-console.log(getNumber(-1));
+getNumber('2023 год');
+getNumber('ECMAScript 2022');
+getNumber('1 кефир, 0.5 батона');
+getNumber('агент 007');
+getNumber('а я томат');
+getNumber(true);
+getNumber(1.5);
+getNumber(-1);
